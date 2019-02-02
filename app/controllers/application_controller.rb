327a16f60f817c25f0dbe3b877d39a9171852f1a
Sphinx <mridulnagpal07@gmail.com>
2019-02-02 18:03:07 +0530

    Font fixed, Year changed in footer, twitter links changed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -49,7 +49,7 @@ class ApplicationController < ActionController::Base
   end
 
   def request_login
-    flash[:info] = 'Please log into crowdAI to perform this action.'
+    flash[:info] = 'Please log into AIcrowd to perform this action.'
     redirect_to new_participant_session_path
   end
 
