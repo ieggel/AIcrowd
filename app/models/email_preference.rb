bc1136ac052a26a811b4c7c72e910ff3c94914d8
Nate Jenkins <nate.jenkins@gmail.com>
2019-02-11 00:41:08 +0100

    cleanup

--- app/models/email_preference.rb
+++ app/models/email_preference.rb
@@ -8,7 +8,7 @@ class EmailPreference < ApplicationRecord
   def set_defaults
     # NATE: I don't like the default of true but this is only to make the tests pass and should
     # not occur in production since the email_preference is built after a user is created.
-    self.newsletter = self.participant && self.participant.agreed_to_marketing || true
+    self.newsletter = self.participant ? self.participant.agreed_to_marketing : true
     self.challenges_followed = true
     self.mentions = true
     self.email_frequency = :daily
